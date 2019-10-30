3f613ae4bba215524b3cba65f3a430692140a7c7
Vercillius <vercillius.mila@fligno.com>
2019-10-31 03:14:22 +0800

    initial transaction history UI

--- surra-mobile/src/transaction-history/index.js
+++ surra-mobile/src/transaction-history/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, ScrollView,TouchableOpacity} from 'react-native';
+import {View, Text, ScrollView,TouchableOpacity, Picker, FlatList} from 'react-native';
 import {connect} from 'react-redux';
 
 
@@ -10,12 +10,51 @@ class TransactionHistory extends Component {
 
   render() {
     return (
-      <ScrollView>
-        <TouchableOpacity onPress={()=> this.props.navigation.navigate('Pickup')}>
-        <Text>Transaction History</Text>
-        </TouchableOpacity>
-        
-      </ScrollView>
+      <View
+        style={{
+          backgroundColor: '#A4B73B',
+          flex: 1,
+          paddingHorizontal: 30,
+          paddingTop: 100,
+          justifyContent: 'space-between',
+        }}>
+        <View>
+          <Text style={{color: '#fff', fontSize: 20, fontWeight: 'bold'}}>
+            Transaction History
+          </Text>
+
+          <FlatList
+            style={{marginTop:50}}
+            data={this.props.pickup.items}
+            keyExtractor={(item, index) => {
+              return index + '';
+            }}
+            renderItem={({item, index}) => {
+              return (
+                <View style={{borderBottomColor:'#849524', borderBottomWidth:1, paddingVertical:20}}>
+                  <Text style={{color:'#fff'}}>Type: {item.type}</Text>
+                  <Text  style={{color:'#fff'}}>Weight: {item.weight}</Text>
+                </View>
+              );
+            }}
+          />
+        </View>
+        <View style={{paddingVertical: 30}}>
+          <TouchableOpacity
+            style={{
+              width: '100%',
+              borderRadius: 5,
+              backgroundColor: '#265D0C',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            onPress={() =>
+              this.props.navigation.navigate('TransactionHistory')
+            }>
+            <Text style={{color: '#fff', padding: 15}}>Submit</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
